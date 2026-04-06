4817b9401424a1af3ad019b58139fee8b0f99541
Manthan Garg <manthangarg2024cse@gmail.com>
2026-04-06 20:04:08 +0000

    datypes_learning

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+// alert("Hello") // in nodejs there is another way to use this  , this is browser method 
+
+console.log(3 + 3) 
+console.log("Mathan")
+
+// original documentation of java is available on tc39.es
+
+let name = "Manthan"
+let age = 19
+let isLoggedIn = false
+let state
+let data = null
+// datatypes
+
+// number -> 2 to the power 53
+// bigint 
+// string
+// boolean
+// null -> standalone value
+// undefined
+// symbol -> uniqueness
+// object
+console.log(typeof "manthan");
+console.log(typeof age);
+console.log(typeof null);  // iska type hai object
+console.log(typeof undefined);  //undefined
