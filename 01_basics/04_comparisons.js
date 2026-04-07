b592e60c2df679b031f9b0886682dd44a694239d
Manthan Garg <manthangarg2024cse@gmail.com>
2026-04-07 06:46:09 +0000

    comparisons_understanding

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,28 @@
+// simple camparisons
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null < 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined < 0);
+
+
+// === > strict check (value and datatype both)
+
+console.log("2"==2) //returns true as only value is compared
+console.log("2"===2);  //as datatype is also compared so false comes
